574e7f2634c9c0e6a7b7401b6ae4ffe56e07064a
Vladimir <vvkraf@gmail.com>
2017-03-25 02:36:58 +0300

    UI : add several menus, switching

---
+++ Game/UI/Pages/AwaitingPageOptions.cs
@@ -0,0 +1,31 @@
+﻿using Fusion.Core.Mathematics;
+using Fusion.Engine.Common;
+using Fusion.Engine.Graphics;
+using IronStar.UI.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IronStar.UI.Pages
+{
+    public class AwaitingPageOptions : IPageOption
+    {
+        private readonly Game game;
+        public AwaitingPageOptions(Game game)
+        {
+            this.game = game;
+        }
+
+        [Background(0, "Background", @"ui\background")]
+        public void Background() { }
+
+        [Logo(1,"Logo", @"ui\logo")]
+        public void Logo() { }
+
+        [StartLabel(2,"Label", "Awaiting world ... ")]
+        public string Label;
+
+    }
+}

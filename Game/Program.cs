4433b9c17631841995ee1ca6093dc44d4683359c
demiurghg <demiurghg@gmail.com>
2017-05-21 22:41:22 +0300

    NLua

--- Game/Program.cs
+++ Game/Program.cs
@@ -20,6 +20,7 @@ using Fusion.Build.Mapping;
 using Fusion.Engine.Client;
 using Fusion.Engine.Server;
 using IronStar.Editor2;
+using NLua;
 
 namespace IronStar {
 
@@ -74,6 +75,14 @@ namespace IronStar {
 			//Allocator2D.RunTest(512, 1024, @"C:\GITHUB\_alloc_test");
 			//return 0; 
 
+			//using ( var L = new Lua() ) {
+			//	L.LoadCLRPackage();
+			//	L.RegisterFunction("blah", 
+			//	L.DoFile("form.lua");
+			//	//L.DoString("print(5)");
+			//}
+
+			//return 0;
 
 			//
 			//	Build content on startup.

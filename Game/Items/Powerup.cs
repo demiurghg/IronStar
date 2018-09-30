5f0064d76ac4554afe40a9191b4306b0719ed946
demiurghg <demiurghg@gmail.com>
2018-09-30 17:23:11 +0300

    Health & armor power-ups

---
+++ Game/Items/Powerup.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Fusion.Core;
+using IronStar.Core;
+using IronStar.Entities;
+using IronStar.Entities.Players;
+
+namespace IronStar.Items {
+	public class Powerup : Item {
+
+		readonly GameWorld world;
+
+		readonly int bonusHealth;
+		readonly int bonusArmor;
+		
+
+		public Powerup( uint id, short clsid, GameWorld world, PowerupFactory factory ) : base( id, clsid )
+		{
+			this.world			=	world;
+
+			this.bonusArmor		=	factory.ArmorBonus;
+			this.bonusHealth	=	factory.HealthBonus;
+		}
+
+
+		public override bool Pickup( Entity other )
+		{
+			var player = other as Player;
+
+			if ( bonusHealth>0 && player.Health<100 || bonusArmor>0 && player.Armor<100 ) {
+
+				player.Health	=	Math.Min( player.Health	+ bonusHealth, 100 );
+				player.Armor	=	Math.Min( player.Armor	+ bonusArmor , 100 );
+
+				return true;
+
+			} else {
+
+				return false;
+
+			}
+		}
+	}
+}
---
+++ Game/Items/PowerupFactory.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Fusion;
+using Fusion.Core;
+using Fusion.Core.Content;
+using Fusion.Core.Mathematics;
+using Fusion.Engine.Common;
+using Fusion.Core.Input;
+using Fusion.Engine.Client;
+using Fusion.Engine.Server;
+using Fusion.Engine.Graphics;
+using Fusion.Core.Extensions;
+using IronStar.Core;
+using Fusion.Core.Shell;
+using System.IO;
+using BEPUphysics;
+using BEPUphysics.Entities.Prefabs;
+using BEPUphysics.EntityStateManagement;
+using BEPUphysics.PositionUpdating;
+using Fusion.Core.IniParser.Model;
+using System.ComponentModel;
+
+namespace IronStar.Items {
+
+	public class PowerupFactory : ItemFactory {
+
+		[AECategory("Vital Bonus")]
+		public int HealthBonus { get; set; } = 10;
+
+		[AECategory("Vital Bonus")]
+		public int ArmorBonus { get; set; } = 10;
+
+
+		public override Item Spawn( uint id, short clsid, GameWorld world )
+		{
+			return new Powerup( id, clsid, world, this );
+		}
+	}
+}

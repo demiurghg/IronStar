d56b07dcb01f17f7626c4d6cd65a3e2ef13981cc
demiurghg <demiurghg@gmail.com>
2019-02-13 01:34:27 +0300

    UV Unwrap uncompleted stuff

---
+++ Engine/Engine/Graphics/Scene/Unwrapper.UVShell.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Fusion.Core.Mathematics;
+using QuickGraph;
+
+namespace Fusion.Engine.Graphics {
+	public partial class Unwrapper {
+
+		/// <summary>
+		/// Represents UV shell
+		/// </summary>
+		public class UVShell {
+
+			readonly Mesh mesh;
+			readonly UVTriangle[] tris;
+
+
+			/// <summary>
+			/// Creates instance of UV shell.
+			/// </summary>
+			public UVShell ( Mesh mesh, IEnumerable<UVTriangle> tris )
+			{
+				this.tris	=	tris.ToArray();
+				this.mesh	=	mesh;
+			}
+
+
+
+			public void BuildTopology ()
+			{
+				var dict = new Dictionary<Tuple<Vector2,Vector2>, UVTriangle>();
+
+				foreach ( var tri in tris ) {
+					dict.Add( new Tuple<Vector2, Vector2>( tri.TexCoords[0], tri.TexCoords[1] ), tri );
+					dict.Add( new Tuple<Vector2, Vector2>( tri.TexCoords[1], tri.TexCoords[2] ), tri );
+					dict.Add( new Tuple<Vector2, Vector2>( tri.TexCoords[2], tri.TexCoords[0] ), tri );
+				}
+
+				foreach ( var tri in tris ) {
+
+					var pair0	=	new Tuple<Vector2, Vector2>( tri.TexCoords[1], tri.TexCoords[0] );
+					var pair1	=	new Tuple<Vector2, Vector2>( tri.TexCoords[2], tri.TexCoords[1] );
+					var pair2	=	new Tuple<Vector2, Vector2>( tri.TexCoords[0], tri.TexCoords[2] );
+
+					UVTriangle other;
+
+					if ( dict.TryGetValue( pair0, out other ) ) {
+						tri.Neighbours[0]	=	other;
+					}
+
+					if ( dict.TryGetValue( pair1, out other ) ) {
+						tri.Neighbours[1]	=	other;
+					}
+
+					if ( dict.TryGetValue( pair2, out other ) ) {
+						tri.Neighbours[2]	=	other;
+					}
+				}
+			}
+			
+		}
+	}
+}
--- Engine/Engine/Graphics/Scene/Unwrapper.cs
+++ Engine/Engine/Graphics/Scene/Unwrapper.cs
@@ -8,10 +8,12 @@ using Fusion.Core.Mathematics;
 namespace Fusion.Engine.Graphics {
 	public partial class Unwrapper {
 
-		readonly Mesh			targetMesh;
-		readonly int[]			axisMapping;
 		readonly MeshVertex[]	vertices;
 		readonly MeshTriangle[]	triangles;
+
+		readonly UVTriangle[]	uvTriangles;
+
+		readonly List<UVShell>	uvShells = new List<UVShell>();
 
 
 		/// <summary>
@@ -21,29 +23,51 @@ namespace Fusion.Engine.Graphics {
 		/// <param name="projectionScale"></param>
 		public Unwrapper ( Mesh mesh, float projectionScale )
 		{
-			this.targetMesh		=	mesh;
+			//	separate triangles
+			mesh.SeparateTriangles();
 
 			vertices	=	mesh.Vertices.ToArray();
 			triangles	=	mesh.Triangles.ToArray();
-			axisMapping	=	new int[mesh.TriangleCount];
+			uvTriangles	=	new UVTriangle[ triangles.Length ];
+
+			//	initial projection :			
+			for ( int i=0; i<triangles.Length; i++ ) {
+				uvTriangles[i]	=	ProjectUVs( mesh, i );
+			}
+
+			//	get initial shells :
+			uvShells.Add( new UVShell( mesh, uvTriangles.Where( tri => tri.Projection==0 ) ) );
+			uvShells.Add( new UVShell( mesh, uvTriangles.Where( tri => tri.Projection==1 ) ) );
+			uvShells.Add( new UVShell( mesh, uvTriangles.Where( tri => tri.Projection==2 ) ) );
+
+			foreach ( var shell in uvShells ) {
+				shell.BuildTopology();
+			}
+
+			//	write vertices back
+			for ( int i=0; i<triangles.Length; i++ ) {
+				
+				var tri	=	mesh.Triangles[i];
+				
+				var v0	=	mesh.Vertices[ tri.Index0 ]; 
+				var v1	=	mesh.Vertices[ tri.Index1 ]; 
+				var v2	=	mesh.Vertices[ tri.Index2 ]; 
+
+				v0.TexCoord0	=	uvTriangles[i].TexCoords[0];
+				v1.TexCoord0	=	uvTriangles[i].TexCoords[1];
+				v2.TexCoord0	=	uvTriangles[i].TexCoords[2];
+
+				mesh.Vertices[ tri.Index0 ] = v0;
+				mesh.Vertices[ tri.Index1 ] = v1;
+				mesh.Vertices[ tri.Index2 ] = v2;
+			}
+
+			//	merge vertices back :
+			mesh.MergeVertices(0);
 		}
 
 
 	
-		/// <summary>
-		/// 
-		/// </summary>
-		public void Build ()
-		{
-			for ( int i=0; i<triangles.Length; i++ ) {
-
-				axisMapping[i]	=	ProjectUVs( i );
-
-			}
-
-			targetMesh.Vertices	=	vertices.ToList();
-		}
-
 
 
 		/// <summary>
@@ -51,69 +75,60 @@ namespace Fusion.Engine.Graphics {
 		/// </summary>
 		/// <param name="triIndex"></param>
 		/// <returns></returns>
-		int ProjectUVs ( int triIndex )
+		UVTriangle ProjectUVs ( Mesh mesh, int triIndex )
 		{
-			var tri	=	triangles[ triIndex ];
-
-			var p0	=	targetMesh.Vertices[ tri.Index0 ].Position;
-			var p1	=	targetMesh.Vertices[ tri.Index1 ].Position;
-			var p2	=	targetMesh.Vertices[ tri.Index2 ].Position;
-
-			var v01	=	p1 - p0;
-			var v02	=	p2 - p0;
-
-			var n	=	Vector3.Cross( v01, v02 );
-
-			var nn	=	Vector3.Normalize( n );
+			var tri		=	triangles[ triIndex ];
+			var nn		=	tri.ComputeNormal( mesh );
 
 			var absX	=	Math.Abs( nn.X );
 			var absY	=	Math.Abs( nn.Y );
 			var absZ	=	Math.Abs( nn.Z );
 
+			Vector2 tc0, tc1, tc2;
+
 			if ( absX >= absY && absX >= absZ ) 
 			{
-				vertices[ tri.Index0 ].TexCoord0.X	=	vertices[ tri.Index0 ].Position.Y;
-				vertices[ tri.Index0 ].TexCoord0.Y	=	vertices[ tri.Index0 ].Position.Z;
+				tc0.X	=	vertices[ tri.Index0 ].Position.Y;
+				tc0.Y	=	vertices[ tri.Index0 ].Position.Z;
 
-				vertices[ tri.Index1 ].TexCoord0.X	=	vertices[ tri.Index1 ].Position.Y;
-				vertices[ tri.Index1 ].TexCoord0.Y	=	vertices[ tri.Index1 ].Position.Z;
+				tc1.X	=	vertices[ tri.Index1 ].Position.Y;
+				tc1.Y	=	vertices[ tri.Index1 ].Position.Z;
 
-				vertices[ tri.Index2 ].TexCoord0.X	=	vertices[ tri.Index2 ].Position.Y;
-				vertices[ tri.Index2 ].TexCoord0.Y	=	vertices[ tri.Index2 ].Position.Z;
-				
-				return 0;
+				tc2.X	=	vertices[ tri.Index2 ].Position.Y;
+				tc2.Y	=	vertices[ tri.Index2 ].Position.Z;
+
+				return new UVTriangle( 0, triIndex, tc0, tc1, tc2 );
 			}
 			
 			if ( absY >= absX && absY >= absZ ) 
 			{
-				vertices[ tri.Index0 ].TexCoord0.X	=	vertices[ tri.Index0 ].Position.X;
-				vertices[ tri.Index0 ].TexCoord0.Y	=	vertices[ tri.Index0 ].Position.Z;
+				tc0.X	=	vertices[ tri.Index0 ].Position.X;
+				tc0.Y	=	vertices[ tri.Index0 ].Position.Z;
 
-				vertices[ tri.Index1 ].TexCoord0.X	=	vertices[ tri.Index1 ].Position.X;
-				vertices[ tri.Index1 ].TexCoord0.Y	=	vertices[ tri.Index1 ].Position.Z;
+				tc1.X	=	vertices[ tri.Index1 ].Position.X;
+				tc1.Y	=	vertices[ tri.Index1 ].Position.Z;
 
-				vertices[ tri.Index2 ].TexCoord0.X	=	vertices[ tri.Index2 ].Position.X;
-				vertices[ tri.Index2 ].TexCoord0.Y	=	vertices[ tri.Index2 ].Position.Z;
-				
-				return 1;
+				tc2.X	=	vertices[ tri.Index2 ].Position.X;
+				tc2.Y	=	vertices[ tri.Index2 ].Position.Z;
+
+				return new UVTriangle( 1, triIndex, tc0, tc1, tc2 );
 			}
 			
 			if ( absZ >= absX && absZ >= absY ) 
 			{
-				vertices[ tri.Index0 ].TexCoord0.X	=	vertices[ tri.Index0 ].Position.X;
-				vertices[ tri.Index0 ].TexCoord0.Y	=	vertices[ tri.Index0 ].Position.Y;
+				tc0.X	=	vertices[ tri.Index0 ].Position.X;
+				tc0.Y	=	vertices[ tri.Index0 ].Position.Y;
 
-				vertices[ tri.Index1 ].TexCoord0.X	=	vertices[ tri.Index1 ].Position.X;
-				vertices[ tri.Index1 ].TexCoord0.Y	=	vertices[ tri.Index1 ].Position.Y;
+				tc1.X	=	vertices[ tri.Index1 ].Position.X;
+				tc1.Y	=	vertices[ tri.Index1 ].Position.Y;
 
-				vertices[ tri.Index2 ].TexCoord0.X	=	vertices[ tri.Index2 ].Position.X;
-				vertices[ tri.Index2 ].TexCoord0.Y	=	vertices[ tri.Index2 ].Position.Y;
-				
-				return 2;
+				tc2.X	=	vertices[ tri.Index2 ].Position.X;
+				tc2.Y	=	vertices[ tri.Index2 ].Position.Y;
+
+				return new UVTriangle( 2, triIndex, tc0, tc1, tc2 );
 			}
 
 			throw new InvalidOperationException("ProjectUVs failed");
-			
 		}
 	}
 }

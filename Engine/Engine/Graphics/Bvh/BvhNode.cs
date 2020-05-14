e44bbf93eacdf45f687ea9803f77cd20b919042d
demiurghg <demiurghg@gmail.com>
2020-05-15 00:29:32 +0300

    BVH tree initial stuff

---
+++ Engine/Engine/Graphics/Bvh/BvhNode.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Fusion.Core.Mathematics;
+
+namespace Fusion.Engine.Graphics.Bvh
+{
+	public sealed class BvhNode<TPrimitive>
+	{
+		public readonly bool IsLeaf;
+		public readonly TPrimitive Primitive;
+		public readonly BoundingBox BoundingBox;
+
+		public readonly BvhNode<TPrimitive>[] Children = new BvhNode<TPrimitive>[2];
+
+		BvhNode() {}
+
+		public BvhNode ( TPrimitive primitive, BoundingBox bbox )
+		{
+			this.IsLeaf			=	true;
+			this.Primitive		=	primitive;
+			this.BoundingBox	=	bbox;
+		}
+
+		public BvhNode ( BoundingBox bbox )
+		{
+			this.IsLeaf			=	false;
+			this.Primitive		=	default(TPrimitive);
+			this.BoundingBox	=	bbox;
+		}
+	}
+}
---
+++ Engine/Engine/Graphics/Bvh/BvhTree.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Fusion.Core.Mathematics;
+
+namespace Fusion.Engine.Graphics.Bvh
+{
+	public static class BvhTree
+	{
+		public static BvhNode<T> Construct<T> ( IEnumerable<T> primitives, Func<T,BoundingBox> bboxSelector, Func<T,Vector3> centroidSelector )
+		{
+			var sortedData	=	primitives
+					.Select( p0 => new { 
+						Primitive = p0, 
+						BoundingBox = bboxSelector(p0), 
+						Centroid = centroidSelector(p0),
+						ZOrder = ComputeZOrder( centroidSelector(p0)
+					})
+					.OrderBy( p1 => p1.ZOrder )
+					.ToArray();
+
+			
+
+		}
+
+
+		static ulong ComputeZOrder ( Vector3 point )
+		{
+			const int scale = 128;
+			const int bias  = 8192;
+			Int3 intPoint = new Int3( 
+				(int)(point.X * scale + bias),
+				(int)(point.Y * scale + bias),
+				(int)(point.Z * scale + bias) );
+
+			return MortonCode.Code3Uint64( intPoint );
+		}
+
+
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <typeparam name="T"></typeparam>
+		/// <param name="root">Root of BVH tree</param>
+		/// <param name="selector"></param>
+		/// <returns></returns>
+		public static IEnumerable<T> Traverse<T>( BvhNode<T> root, Func<T,BoundingBox,ContainmentType> selector )
+		{
+			throw new NotImplementedException();
+		}
+	}
+}

7636fb241e6c5ef43fd26ddea65875d1f2704840
demiurghg <demiurghg@gmail.com>
2020-05-14 22:21:58 +0300

    Morton code for 64-bit integer

--- Engine/Core/Mathematics/MortonCode.cs
+++ Engine/Core/Mathematics/MortonCode.cs
@@ -64,24 +64,25 @@ namespace Fusion.Core.Mathematics
 			return x;
 		}		
 
+		static ulong SeparateBy2(ulong x) 
+		{
+			x &= 0x1fffff;
+			x = (x | x << 32) & 0x001f00000000ffff;
+			x = (x | x << 16) & 0x001f0000ff0000ff;
+			x = (x | x <<  8) & 0x100f00f00f00f00f;
+			x = (x | x <<  4) & 0x10c30c30c30c30c3;
+			x = (x | x <<  2) & 0x1249249249249249;
+			return x;
+		}		
+
 		static uint CompactBy2(uint x) 
 		{	
-										        
 			x &=                  0x09249249;	// 0000 1001 0010 0100 1001 0010 0100 1001
 			x = (x ^ (x >> 2))  & 0x030C30C3;	// 0000 0011 0000 1100 0011 0000 1100 0011
 			x = (x ^ (x >> 4))  & 0x0300f00f;	// 0000 0011 0000 0000 1111 0000 0000 1111
 			x = (x ^ (x >> 8))  & 0x030000ff;	// 0000 0011 0000 0000 0000 0000 1111 1111
 			x = (x ^ (x >> 16)) & 0x000003ff;	// 0000 0000 0000 0000 0000 0011 1111 1111
-
 			return x;
-
-			//	}
-			//	x &= 0x55555555;                  // x = -f-e -d-c -b-a -9-8 -7-6 -5-4 -3-2 -1-0
-			//x = (x ^ (x >>  1)) & 0x33333333; // x = --fe --dc --ba --98 --76 --54 --32 --10
-			//x = (x ^ (x >>  2)) & 0x0f0f0f0f; // x = ---- fedc ---- ba98 ---- 7654 ---- 3210
-			//x = (x ^ (x >>  4)) & 0x00ff00ff; // x = ---- ---- fedc ba98 ---- ---- 7654 3210
-			//x = (x ^ (x >>  8)) & 0x0000ffff; // x = ---- ---- ---- ---- fedc ba98 7654 3210
-			//return x;
 		}
 
 		public static uint Code3(Int3 xyz) 
@@ -101,6 +102,15 @@ namespace Fusion.Core.Mathematics
 			return new Int3( (int)x, (int)y, (int)z );
 		}
 
+
+
+		public static ulong Code3Uint64(Int3 xyz)
+		{
+			ulong x = (ulong)xyz.X;
+			ulong y = (ulong)xyz.Y;
+			ulong z = (ulong)xyz.Z;
+			return SeparateBy2(x) | (SeparateBy2(y) << 1) | (SeparateBy2(z) << 2);
+		}
 
 	}
 }

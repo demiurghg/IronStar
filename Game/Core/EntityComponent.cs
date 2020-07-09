f1fb0bcb59302ed30f1feb6a28cfab46ae287434
demiurghg <demiurghg@gmail.com>
2020-07-09 20:57:54 +0300

    Some code cleanup

---
+++ Game/Core/EntityComponent.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Fusion;
+using Fusion.Core;
+using Fusion.Core.Utils;
+using Fusion.Core.Mathematics;
+using Fusion.Core.Extensions;
+using System.IO;
+using IronStar.SFX;
+using Fusion.Engine.Graphics;
+using Fusion.Core.Content;
+using Fusion.Engine.Common;
+using IronStar.Views;
+
+namespace IronStar.Core 
+{
+	/// <summary>
+	/// Entity component.
+	/// </summary>
+	public abstract class EntityComponent : DisposableBase
+	{
+		public virtual void Update ( GameTime gameTime ) {}
+	}
+}

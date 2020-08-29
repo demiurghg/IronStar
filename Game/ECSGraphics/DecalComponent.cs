ad1109110f0daf1d1449d55249a72b43f49ea3cd
demiurghg <demiurghg@gmail.com>
2020-08-29 13:47:47 +0300

    Decal component and system

---
+++ Game/ECSGraphics/DecalComponent.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Fusion.Core.Mathematics;
+using IronStar.ECS;
+using Fusion.Engine.Graphics;
+using RSSpotLight = Fusion.Engine.Graphics.SpotLight;
+using Fusion.Core.Shell;
+
+namespace IronStar.SFX2
+{
+	public class DecalComponent : Component
+	{
+		[AECategory("Decal Image")]
+		[AEAtlasImage(@"decals/decals")]
+		public string ImageName { get; set; } = "";
+
+		/// <summary>
+		/// 
+		/// </summary>
+		[AECategory("Decal Size")]
+		[AEValueRange(0, 64, 1f, 1/16f)]
+		public float Width { get; set;} = 4;
+
+		/// <summary>
+		/// 
+		/// </summary>
+		[AECategory("Decal Size")]
+		[AEValueRange(0, 64, 1f, 1/16f)]
+		public float Height { get; set;} = 4;
+
+		/// <summary>
+		/// 
+		/// </summary>
+		[AECategory("Decal Size")]
+		[AEValueRange(0, 16, 1f, 1/16f)]
+		public float Depth { get; set;} = 1;
+
+		/// <summary>
+		/// Decal emission intensity
+		/// </summary>
+		[AECategory("Decal Material")]
+		public Color EmissionColor { get; set;} = Color.Black;
+
+		/// <summary>
+		/// Decal emission intensity
+		/// </summary>
+		[AECategory("Decal Material")]
+		public float EmissionIntensity { get; set;} = 100;
+
+		/// <summary>
+		/// Decal base color
+		/// </summary>
+		[AECategory("Decal Material")]
+		public Color BaseColor { get; set;} = new Color(128,128,128,255);
+
+		/// <summary>
+		/// Decal roughness
+		/// </summary>
+		[AECategory("Decal Material")]
+		[AEValueRange(0, 1, 1/4f, 1/128f)]
+		public float Roughness { get; set;}= 0.5f;
+
+		/// <summary>
+		/// Decal meatllic
+		/// </summary>
+		[AECategory("Decal Material")]
+		[AEValueRange(0, 1, 1/4f, 1/128f)]
+		public float Metallic { get; set;} = 0.5f;
+
+		/// <summary>
+		/// Color blend factor [0,1]
+		/// </summary>
+		[AECategory("Decal Material")]
+		[AEValueRange(0, 1, 1/4f, 1/128f)]
+		public float ColorFactor { get; set;} = 1.0f;
+
+		/// <summary>
+		/// Roughmess and specular blend factor [0,1]
+		/// </summary>
+		[AECategory("Decal Material")]
+		[AEValueRange(0, 1, 1/4f, 1/128f)]
+		public float SpecularFactor { get; set;} = 1.0f;
+
+		/// <summary>
+		/// Normalmap blend factor [-1,1]
+		/// </summary>
+		[AECategory("Decal Material")]
+		[AEValueRange(0, 1, 1/4f, 1/128f)]
+		public float NormalMapFactor { get; set;} = 1.0f;
+
+		/// <summary>
+		/// Falloff factor [-1,1]
+		/// </summary>
+		[AECategory("Decal Material")]
+		[AEValueRange(0, 1, 1/4f, 1/128f)]
+		public float FalloffFactor { get; set;} = 0.5f;
+	}
+}
---
+++ Game/ECSGraphics/DecalSystem.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Fusion.Core.Mathematics;
+using IronStar.ECS;
+using Fusion.Engine.Graphics;
+using RSSpotLight = Fusion.Engine.Graphics.SpotLight;
+using Fusion.Core.Shell;
+using Fusion.Core;
+
+namespace IronStar.SFX2
+{
+	public class DecalSystem : ProcessingSystem<Decal,DecalComponent,Transform>
+	{
+		Dictionary<uint,Decal> lights = new Dictionary<uint, Decal>();
+
+		readonly LightSet ls;
+
+		
+		public DecalSystem( RenderSystem rs )
+		{
+			this.ls	=	rs.RenderWorld.LightSet;
+		}
+
+		
+		protected override Decal Create( Entity e, DecalComponent ol, Transform t )
+		{
+			var decal = new Decal();
+
+			Process( e, GameTime.Zero, decal, ol, t );
+
+			ls.Decals.Add( decal );
+			return decal;
+		}
+
+		
+		protected override void Destroy( Entity e, Decal decal )
+		{
+			ls.Decals.Remove( decal );
+		}
+
+		
+		protected override void Process( Entity e, GameTime gameTime, Decal decal, DecalComponent dc, Transform t )
+		{
+			var transform			=	t.TransformMatrix;
+
+			decal.DecalMatrix		=	Matrix.Scaling( dc.Width/2, dc.Height/2, dc.Depth/2 ) * transform;
+			decal.DecalMatrixInverse=	Matrix.Invert( decal.DecalMatrix );
+									
+			decal.Emission			=	dc.EmissionColor.ToColor4() * MathUtil.Exp2( dc.EmissionIntensity );
+			decal.BaseColor			=	new Color4( dc.BaseColor.R/255.0f, dc.BaseColor.G/255.0f, dc.BaseColor.B/255.0f, 1 );
+			
+			decal.Metallic			=	dc.Metallic;
+			decal.Roughness			=	dc.Roughness;
+			decal.ImageRectangle	=	ls.DecalAtlas.GetNormalizedRectangleByName( dc.ImageName );
+			decal.ImageSize			=	ls.DecalAtlas.GetAbsoluteRectangleByName( dc.ImageName ).Size;
+
+			decal.ColorFactor		=	dc.ColorFactor;
+			decal.SpecularFactor	=	dc.SpecularFactor;
+			decal.NormalMapFactor	=	dc.NormalMapFactor;
+			decal.FalloffFactor		=	dc.FalloffFactor;
+
+			decal.Group				=	InstanceGroup.Static;
+		}
+	}
+}

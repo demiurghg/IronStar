2964db5e0bf0d41d9bca82ee7cd31688e28e666b
demiurghg <demiurghg@gmail.com>
2020-09-07 02:33:19 +0300

    Monster class

---
+++ Game/ECSFactories/MonsterFactory.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Fusion.Core.Mathematics;
+using IronStar.ECS;
+using IronStar.ECSPhysics;
+using IronStar.Gameplay;
+using IronStar.Gameplay.Components;
+using IronStar.SFX2;
+
+namespace IronStar.ECSFactories
+{
+	[EntityFactory("MONSTER_MARINE")]
+	public class MonsterMarineFactory : EntityFactory
+	{
+		public override Entity Spawn( GameState gs )
+		{
+			var e = gs.Spawn();
+
+			//e.AddComponent( new PlayerComponent() );
+			e.AddComponent( new RenderModel("scenes\\monsters\\marine\\marine", 0.1f, Color.Red, 7, RMFlags.None ) );
+			e.AddComponent( new CharacterController(6,4,2, 24,9, 20, 10, 2.2f) );
+			e.AddComponent( new UserCommandComponent() );
+			e.AddComponent( new Transform() );
+			e.AddComponent( new Velocity() );
+			e.AddComponent( new StepComponent() );
+
+			e.AddComponent( new InventoryComponent() );
+
+			return e;
+		}
+	}
+}

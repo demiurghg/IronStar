479a12db959ba03aa6fc3cac083ed55efc3ae95a
demiurghg <demiurghg@gmail.com>
2021-02-12 01:49:04 +0300

    Data provider interfaces for GI

---
+++ Engine/Engine/Graphics/GI2/ILightProbeProvider.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Fusion.Drivers.Graphics;
+
+namespace Fusion.Engine.Graphics.GI2
+{
+	internal interface ILightProbeProvider
+	{
+		ShaderResource	GetLightProbeCubeArray();
+		int				GetLightProbeIndex( Guid guid );
+	}
+}
---
+++ Engine/Engine/Graphics/GI2/ILightmapProvider.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Fusion.Core.Mathematics;
+using Fusion.Drivers.Graphics;
+
+namespace Fusion.Engine.Graphics.GI2
+{
+	internal interface ILightmapProvider
+	{
+		Size2		LightmapSize { get; }
+		BoundingBox VolumeBounds { get; }
+
+		ShaderResource	LightmapL0 { get; }
+		ShaderResource	LightmapL1 { get; }
+		ShaderResource	LightmapL2 { get; }
+		ShaderResource	LightmapL3 { get; }
+
+		ShaderResource	VolumeL0 { get; }
+		ShaderResource	VolumeL1 { get; }
+		ShaderResource	VolumeL2 { get; }
+		ShaderResource	VolumeL3 { get; }
+	}
+}

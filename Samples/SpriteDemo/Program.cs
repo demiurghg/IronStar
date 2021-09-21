f07def3e157580d403fd8af2404266d1a5574353
demiurghg <demiurghg@gmail.com>
2021-09-22 01:27:46 +0300

    SpriteDemo

---
+++ Samples/SpriteDemo/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using Fusion;
+using Fusion.Build;
+using Fusion.Build.Processors;
+using Fusion.Core.Utils;
+
+namespace SpriteDemo
+{
+	class Program
+	{
+		static void Main( string[] args )
+		{
+			// 	colored console output :
+			Log.AddListener( new ColoredLogListener() );
+
+			//	output for in-game console :
+			Log.AddListener( new LogRecorder() );
+
+			//	set verbosity :
+			Log.VerbosityLevel = LogMessageType.Verbose;
+
+			//
+			//	Build content on startup.
+			//	Remove this line in release code.
+			//
+			var builder = CreateBuilder();
+			builder.Build();
+
+			//
+			//	Run game :
+			//
+			using (var game = new SpriteDemo("SpriteDemo", "SpriteDemo")) 
+			{
+				game.RenderSystem.UseDebugDevice	=	false;	//	do not use debug graphics device
+				game.RenderSystem.Fullscreen		=	false;	//	no not use fullscreen mode on startup
+				game.RenderSystem.UseRenderDoc		=	false;	//	do not use debug graphics device
+
+				game.TrackObjects					=	false;	//	do not track objects
+
+				//	run game
+				game.Run();
+			}
+		}
+
+		static Builder CreateBuilder()
+		{
+			//	Create content processors.
+			//	For this sample we need only texture and shader processors:
+			var textureProcessor		=	new TextureProcessor();
+			var ubershaderProcessor		=	new UbershaderProcessor();
+
+			//	create content builder with specified output directory
+			var builder =   new Builder("Content")
+
+				//	provide content and temporary directories :
+				.InputDirectory(@"..\..\..\Content")
+				.TempDirectory (@"..\..\..\Temp")
+				
+				//	set file masks to ignore :
+				.Ignore("*.auto.hlsl" )
+
+				//	provide directories for command line tools :
+				.ToolsDirectory(@"..\..\..\..\..\Engine\SDKs")
+				.ToolsDirectory(@"..\..\..\..\..\Engine\Native\FScene\bin\x64\Release")
+				.ToolsDirectory(@"..\..\..\..\..\Engine\SDKs\KopiLua\Luac\bin\x64\Release")
+
+				//	provide additional content directories :
+				.InputDirectory(@"..\..\..\..\..\Engine\Shaders")
+
+				//	associate file extensions with processors :
+				.Process("*.hlsl"		, ubershaderProcessor )
+				.Process("*.tga"		, textureProcessor )
+				.Process("*.png"		, textureProcessor )
+				.Process("*.jpg"		, textureProcessor )
+				;
+
+			return builder;
+		}
+	}
+}
---
+++ Samples/SpriteDemo/SpriteDemo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Fusion.Core;
+using Fusion.Core.Extensions;
+using Fusion.Engine.Graphics;
+
+namespace SpriteDemo
+{
+	class SpriteDemo : Game
+	{
+		public SpriteDemo( string gameId, string gameTitle ) : base( gameId, gameTitle )
+		{
+			this.AddServiceAndComponent( 100, new RenderSystem(this, true) );
+		}
+	}
+}
